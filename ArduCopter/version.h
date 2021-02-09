8147abffb6dbcbb4ccd4c52a3c4dd364e1f3deff
Charlie-Burge <charlie.burge@manna.aero>
2021-02-09 12:44:10 +0000

    Copter: increase version

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.0.5 - 1.5.2"
+#define THISFIRMWARE "ArduCopter V4.0.5 - 1.5.3"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,5,FIRMWARE_VERSION_TYPE_OFFICIAL
