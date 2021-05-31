08e2f594275453b3c86bf6fd8d6a17e21479517b
Iampete1 <iampete@hotmail.co.uk>
2021-05-31 02:02:55 +0100

    remove param file commands from ride_allog not supported on 4.0

--- Tools/scripts/run_ride_allong.sh
+++ Tools/scripts/run_ride_allong.sh
@@ -1,8 +1,8 @@
 # This script launches three instances of AP and sets the aproprate setup commands, it also loads the same params to each vehicle
 # use sleeps to ensure we launch in the correct order
 
-x-terminal-emulator -e "sleep 5; ./Tools/autotest/sim_vehicle.py -v ArduCopter -f json:0.0.0.0 -I1 --add-param-file=I0.param"
+x-terminal-emulator -e "sleep 5; ./Tools/autotest/sim_vehicle.py -v ArduCopter -f json:0.0.0.0 -I1"
 
-x-terminal-emulator -e "sleep 2; ./Tools/autotest/sim_vehicle.py -v ArduCopter -f json:0.0.0.0 -I2 --add-param-file=I0.param"
+x-terminal-emulator -e "sleep 2; ./Tools/autotest/sim_vehicle.py -v ArduCopter -f json:0.0.0.0 -I2"
 
-./Tools/autotest/sim_vehicle.py -v ArduCopter --console --map --slave 2 --add-param-file=I0.param
+./Tools/autotest/sim_vehicle.py -v ArduCopter --console --map --slave 2
