9134a964c947cc63a84bebb99f6a8c72ddac8af9
Charlie-Burge <charlie.burge@manna.aero>
2021-07-06 17:48:37 +0100

    scripts: update ride along

--- Tools/scripts/run_ride_allong.sh
+++ Tools/scripts/run_ride_allong.sh
@@ -1,8 +1,11 @@
 # This script launches three instances of AP and sets the aproprate setup commands, it also loads the same params to each vehicle
 # use sleeps to ensure we launch in the correct order
 
-x-terminal-emulator -e "sleep 5; ./Tools/autotest/sim_vehicle.py -v ArduCopter -f json:0.0.0.0 -I1"
+VEHICLE="ArduCopter"
+LOCATION="ParkDavis"
 
-x-terminal-emulator -e "sleep 2; ./Tools/autotest/sim_vehicle.py -v ArduCopter -f json:0.0.0.0 -I2"
+x-terminal-emulator -e "sleep 5; ./Tools/autotest/sim_vehicle.py -w -v $VEHICLE -L $LOCATION -f json:0.0.0.0 -I1"
 
-./Tools/autotest/sim_vehicle.py -v ArduCopter --console --map --slave 2
+x-terminal-emulator -e "sleep 2; ./Tools/autotest/sim_vehicle.py -w -v $VEHICLE -L $LOCATION -f json:0.0.0.0 -I2"
+
+./Tools/autotest/sim_vehicle.py -v $VEHICLE -L $LOCATION -w --console --map --slave 2
